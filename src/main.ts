f484ff7720215dc447a6fbbe97ef6354d6f1c3f5
s21014 <soccer0907>
2023-03-29 11:07:19 +0900

    ログイン画面

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+export default class ReportsController {
+  public async bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+    const data = {
+      title: '届け出・証明書申請システム',
+      message: 'ログインしてください',
+    },
+    bootstrap();
+  }
 }
-bootstrap();
